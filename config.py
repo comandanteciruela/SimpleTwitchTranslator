c463ee21a4a47cdf6e1d05e6167781129e29b7b5
comandanteciruela <xkrz@proton.me>
2024-10-12 01:21:49 -0300

    added IGNORE_USERS in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-OAUTH_TOKEN = ""
-CHANNEL_NAME = ""
-BOT_NAME = ""
-BOT_CLIENT_ID = ""
+OAUTH_TOKEN = "get_this_in_twitchtokengenerator_dot_com_access_with_your_bot"
+CHANNEL_NAME = "channel_you_want_the_bot_to_join"
+BOT_NAME = "your_bot_account_nickname"
+BOT_CLIENT_ID = "get_this_in_twitchtokengenerator_dot_com_access_with_your_bot"
 MESSAGES = [
     "Hello, welcome to my channel!",
     "Remember to follow the rules.",
@@ -10,3 +10,4 @@ MESSAGES = [
 MESSAGE_INTERVAL = 2400
 IGNORE_LANG = "es"
 OWNER_TO_PEOPLE = "en"
+IGNORE_USERS = ["johnson_robert_johnson", "robert_von_smith", "dr_alphonse_aubert"]
