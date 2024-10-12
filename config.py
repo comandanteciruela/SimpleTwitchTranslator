d0c4bc27192cc164913091eee7b28a59a0ba0304
comandanteciruela <xkrz@proton.me>
2024-10-12 17:00:07 -0300

    Update config.py with channel lists

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 OAUTH_TOKEN = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
-PRIMARY_CHANNEL_NAME = "channel_you_want_the_bot_to_join"
 BOT_NAME = "your_bot_account_nickname"
 BOT_CLIENT_ID = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
+CHANNELS = [
+    "myfriendlychannel",
+    "yourotherfriendlychannel",
+]
+
+IGNORE_LANG = "es"
+OWNER_TO_PEOPLE = "en"
+
 MESSAGES = [
     "Hello, welcome to my channel!",
     "Remember to follow the rules.",
     "Enjoy!",
 ]
 MESSAGE_INTERVAL = 2400
-IGNORE_LANG = "es"
-OWNER_TO_PEOPLE = "en"
+
 IGNORE_USERS = ["johnson_robert_johnson", "robert_von_smith", "dr_alphonse_aubert"]
