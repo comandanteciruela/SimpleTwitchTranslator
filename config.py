3ac19e3e1a5a54ce12d9085910ff730cc39944af
comandanteciruela <xkrz@proton.me>
2024-10-13 16:41:36 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+# REQUIRED CONFIG
 BOT_OAUTH_TOKEN = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
 BOT_CLIENT_ID = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
 CHANNEL = "yourfriendlychannel"
 
+# OPTIONAL CONFIG
 IGNORE_LANG = "es"
 OWNER_TO_PEOPLE = "en"
-
 WELCOME_MESSAGE = "I am now here! SMOrc"
 MESSAGES = [
     "Hello, welcome to my channel!",
@@ -12,5 +13,4 @@ MESSAGES = [
     "Enjoy!",
 ]
 MESSAGE_INTERVAL = 2400
-
 IGNORE_USERS = ["johnson_robert_johnson", "robert_von_smith", "dr_alphonse_aubert"]
