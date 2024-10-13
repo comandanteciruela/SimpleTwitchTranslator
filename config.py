8d79e10d056aecfd0c2c48142b78cf824a20f748
comandanteciruela <xkrz@proton.me>
2024-10-13 18:08:45 -0300

    BOT_INTRO_MESSAGE to BOT_INTRO_MESSAGES, now a list

--- config.py
+++ config.py
@@ -4,14 +4,18 @@ BOT_CLIENT_ID = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
 CHANNEL = "yourfriendlychannel"
 
 # OPTIONAL CONFIG
+OWNER_TO_PEOPLE = "en"
 IGNORE_LANG = "es"
 IGNORE_TEXT = [
     "I'm eating water.",
     "Fek yoh!",
     "AAAAHHHH!!!!",
 ]
-OWNER_TO_PEOPLE = "en"
-BOT_INTRO_MESSAGE = "Guess who’s back from the void? (๑•̀ㅂ•́)و✧"
+BOT_INTRO_MESSAGES = [
+    "I’ve returned!! Prepare for mayhem! (╯°□°）╯︵ ┻━┻",
+    "Yay! I’m back from the void! (ノ°∀°)ノ",
+    "Guess who’s back to stir some trouble? ( ͡° ͜ʖ ͡°)"
+]
 RANDOM_MESSAGES = [
     "Hello, welcome to my channel!",
     "Remember to follow the rules.",
