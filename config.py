b3fc618768d6ee0b75567eff19fc70482293270d
comandanteciruela <xkrz@proton.me>
2024-10-18 18:11:59 -0300

    Better looking config.py

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
-# REQUIRED CONFIG
+# You should configure the bot by editing this Python configuration file.
+
+# REQUIRED CONFIG (these 5 variables should always be set up)
 BOT_OAUTH_TOKEN = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
 BOT_CLIENT_ID = "get_this_in_twitchtokengenerator_dot_com_connect_your_bot"
 CHANNEL_NAME = "yourfriendlychannel"
 CHANNEL_NATIVE_LANG = "en"
 TRANSLATE_TO_LANG = "es"
 
-# OPTIONAL CONFIG
+"""
+# OPTIONAL CONFIG (not needed, just for additional features if you like)
+
+# LISTS
+
+IGNORE_USERS = [
+    "johnson_robert_johnson",
+    "robert_von_smith",
+    "dr_alphonse_aubert"
+]
 IGNORE_TEXT = [
     "I'm eating water.",
     "Fek yoh!",
@@ -25,10 +36,17 @@ ORDERED_MESSAGES = [
     "Woop woop",
     "Hey",
 ]
+
+# DICT
+
 MESSAGES = {
     "hellobot": "Hello {user}",
     "discord": "My discord invite url is...",
 }
+
+# INTEGER
+
 ORDERED_MESSAGES_INTERVAL = 2400
 RANDOM_MESSAGES_INTERVAL = 2400
-IGNORE_USERS = ["johnson_robert_johnson", "robert_von_smith", "dr_alphonse_aubert"]
+
+"""
